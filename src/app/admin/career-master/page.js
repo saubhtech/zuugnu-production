5c3fbef8383c2587f9ca7c18209548a92ca9657e
saubhtech <saubhtech@gmail.com>
2025-12-31 18:41:17 +0530

    one last try

--- src/app/admin/career-master/page.js
+++ src/app/admin/career-master/page.js
@@ -1,8 +1,6 @@
 
 "use client";
 
-export const dynamic = 'force-dynamic';
-
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import "./career-master.css";
